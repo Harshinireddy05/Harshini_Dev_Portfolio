55dea3cb10241e2eec6218e2770449dd236c4bee
Harshinireddy05 <Harshinireddynellore@gmail.com>
2024-06-05 18:42:40 +0530

    Updates

--- src/NavBar.js
+++ src/NavBar.js
@@ -12,9 +12,9 @@ function NavBar(){
             <ul>
                 <li className='nav-item'><Link to='/'>Home</Link></li>
                 <li className='nav-item'><Link to='/about'>About</Link></li>
-                <li className='nav-item'><a href="/skills">Skills</a></li>
-                <li className='nav-item'><a href="/education">Education</a></li>
-                <li className='nav-item'><a href="/projects">Projects</a></li>
+                <li className='nav-item'><Link to='/skills'>Skills</Link></li>
+                <li className='nav-item'><Link to='/education'>Education</Link></li>
+                <li className='nav-item'><Link to='/projects'>Projects</Link></li>
                 <li className='nav-item'><a href="https://drive.google.com/file/d/13kaPsdMNDsM4LV9g7m5-E5PTildp-yYf/view" target='_blank'>Resume</a></li>
             </ul>
         </nav>
