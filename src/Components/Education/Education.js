1790372c70ca1b3436a24419b82a039f0e460f3d
Harshinireddy05 <Harshinireddynellore@gmail.com>
2024-06-07 12:47:28 +0530

    Education css changes

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -1,34 +1,3 @@
-// import React from 'react';
-// import './Education.css';
-
-// function Education() {
-//   return (
-//     <section className="education">
-//       {/* Title */}
-//       <h2>Education</h2>
-
-//       {/* Education Container (Boxes) */}
-//       <div className="education-container">
-//         {/* School Section */}
-//         <div className="school-box">
-//           <h3>Vignan World One School</h3>
-//           <h4>Visakhapatnam</h4>
-//           <p>Marks: 96%</p>
-//         </div>
-
-//         {/* College Section */}
-//         <div className="college-box">
-//           <h3>BITS Pilani (Batch of 2027)</h3>
-//           <p>CGPA: 8.7</p>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-// export default Education;
-
-
 import React from 'react';
 import './Education.css';
 
